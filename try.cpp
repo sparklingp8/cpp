8d64067bc42e6dac6cc56f145233a359ab5836f6
sparklingp8 <mynewnokiap8@gmail.com>
2023-03-22 19:38:29 +0530

    first commit

---
+++ try.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+   struct students{
+      int roll;
+      string name;
+   };
+   
+   students s1,*s2;
+   s2=&s1;
+   s1.roll=12;
+
+   s1.name="bbbb";
+   cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
+
+
+
+
+}
---
+++ zone.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0,j=20;
+    for (i;i<20;i++){
+        j=i*8;
+     cout<<"I am running "<<i<<" "<<j<<endl;
+    }
+   
+}
