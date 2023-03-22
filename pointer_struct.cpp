26303b18b13b4e75939caa221cdf49a04742a886
sparklingp8 <mynewnokiap8@gmail.com>
2023-03-22 19:55:46 +0530

    pointer file

---
+++ pointer_struct.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+   struct students{
+      int roll;
+      string name;
+   };
+   
+   students s1,*s2;
+   s2=&s1;
+   s1.roll=12;   
+   s1.name="bbbe";
+   cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
+   s2->name="ccce";
+   cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
+
+}
