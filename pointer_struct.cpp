68bf6fd7190433850601e409f562c23b3072d63b
sparklingp8 <mynewnokiap8@gmail.com>
2023-03-22 20:16:46 +0530

    ok

--- pointer_struct.cpp
+++ pointer_struct.cpp
@@ -10,9 +10,9 @@ int main(){
    students s1,*s2;
    s2=&s1;
    s1.roll=12;   
-   s1.name="bbbe";
+   s1.name="hmmm";
    cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
-   s2->name="ccce"; //impoertant
+   s2->name="ooo"; //impoertant
    cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
 
 }
--- try.cpp
+++ try.cpp
@@ -10,9 +10,9 @@ int main(){
    students s1,*s2;
    s2=&s1;
    s1.roll=12;   
-   s1.name="bbbe";
+   s1.name="what hmmm";
    cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
-   s2->name="ccce";
+   s2->name="ooo";
    cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
 
 }
