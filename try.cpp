a8a896641ea0af6c60d79bb6308e51257a271ca4
sparklingp8 <mynewnokiap8@gmail.com>
2023-03-22 19:50:44 +0530

    test2

--- try.cpp
+++ try.cpp
@@ -10,9 +10,9 @@ int main(){
    students s1,*s2;
    s2=&s1;
    s1.roll=12;   
-   s1.name="bbbd";
+   s1.name="bbbe";
    cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
-   s2->name="cccd";
+   s2->name="ccce";
    cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
 
 
