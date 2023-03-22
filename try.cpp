29cb4ebf5a6154c153c26e8f2e031294b8c06db3
sparklingp8 <mynewnokiap8@gmail.com>
2023-03-22 19:41:26 +0530

    changed pointer

--- try.cpp
+++ try.cpp
@@ -9,11 +9,11 @@ int main(){
    
    students s1,*s2;
    s2=&s1;
-   s1.roll=12;
-
+   s1.roll=12;   
    s1.name="bbbb";
    cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
-
+   s2->name="cccc";
+   cout<<s1.name<<s1.roll<<&s1<<&s2<<endl;
 
 
 
