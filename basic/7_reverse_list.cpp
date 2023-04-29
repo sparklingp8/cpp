76a6262026b38847d14fe72558724f196e48d74d
sparklingp8 <mynewnokiap8@gmail.com>
2023-04-29 16:47:02 +0530

    rev_list_code

--- basic/7_reverse_list.cpp
+++ basic/7_reverse_list.cpp
@@ -1,7 +1,35 @@
-#include<iostream>
-using namespace std;
+node *revList(node *h)
+{
+    node *prev, *cur, *nxt = NULL;
 
-int main(){
-    cout<<"to do";
-    return 0;
+    prev = h;
+    cur = prev->next;
+    if (listSize(h) > 2)
+    {
+        nxt = cur->next;
+        prev->next = NULL;
+        while (nxt->next != NULL)
+        {
+            prev = cur;
+            cur = nxt;
+            nxt = nxt->next;
+        }
+        nxt->next = cur;
+        cur->next = prev;
+        h = nxt;
+    }
+    else
+    {
+        prev->next = NULL;
+        cur->next = prev;
+        h = cur;
+        cout << "hsere" << prev;
+    }
+
+    cout << "\n-----------------------------------\n";
+    cout << " List reversed  ";
+    cout << "\n-----------------------------------";
+    seeList(h);
+
+    return h;
 }
