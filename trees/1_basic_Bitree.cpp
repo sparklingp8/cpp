aa4639b46353f19c7f4af3c42b2fe94d640eaa51
sparklingp8 <mynewnokiap8@gmail.com>
2023-05-06 12:03:07 +0530

    trees_codes_dir

---
+++ trees/1_basic_Bitree.cpp
@@ -0,0 +1,11 @@
+//zeeorth first code
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0,j=20;
+    for (i;i<20;i++){
+        j=i*8;
+     cout<<"I am running "<<i<<" "<<j<<endl;
+    }
+   
+}
