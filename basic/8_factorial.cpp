5267e0485b0a3a07b81af7231e7332174318433b
sparklingp8 <mynewnokiap8@gmail.com>
2023-06-29 15:30:06 +0530

    factorial_code

---
+++ basic/8_factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(long n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * (factorial(n - 1));
+}
+
+int main()
+{
+    long i;
+    cout << "\nEnter number to be factorised: ";
+    cin >> i;
+    cout << "\nFactorial of " << i << " is: " << factorial(i) << endl;
+    return 0;
+}
